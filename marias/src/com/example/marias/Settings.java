6dc3501ab1ebe828084bb0df0dfcf4c7f7ba24f6
Maria <soulelemaria@hotmail.com>
2014-11-10 15:54:21 +0200

    db added level field works fine

--- marias/src/com/example/marias/Settings.java
+++ marias/src/com/example/marias/Settings.java
@@ -1,20 +1,37 @@
 package com.example.marias;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 public class Settings extends Activity{
 	
 	Button buttonSound,HomeButton,timerButton;
-	
+	MediaPlayer journey;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.settings);
-	}
+		buttonSound=(ToggleButton)findViewById(R.id.buttonSound);
 	
-}
+	ToggleButton b = (ToggleButton) findViewById(R.id.buttonSound);
+	
+	    }
+	public void onToggleClicked(View view) {
+	    // Is the toggle on?
+	    boolean on = ((ToggleButton) view).isChecked();
+	    
+	    if (on) {
+	        journey.start();
+	    } else {
+	        journey.release();
+	    }
+	}
+	    }   
+
