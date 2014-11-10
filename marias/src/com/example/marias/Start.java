71f52497254ceb5c8c7e94e7b23f4b3b7bd3d18b
Maria <soulelemaria@hotmail.com>
2014-11-10 15:54:21 +0200

    db test of 5 questions working randomly

--- marias/src/com/example/marias/Start.java
+++ marias/src/com/example/marias/Start.java
@@ -1,33 +1,34 @@
 package com.example.marias;
 
+
+
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.ArrayList;
 import java.util.Random;
-import java.util.Set;
+
+import com.example.marias.DbHelper;
+import com.example.marias.Question;
+//import com.example.marias.Start;
+import com.example.marias.R;
+import com.example.marias.ResultActivity;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.LayoutInflater;
+import android.util.Log;
+import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.RadioGroup;
-import android.widget.RadioGroup.OnCheckedChangeListener;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class Start extends Activity {
 
     
 	MediaPlayer journey;
-	private List<String> questionsList;
+/*	private List<String> questionsList;
 	private List<String> answersList;
 	private Map<String,Boolean> regionsMap;
 	private String correctAnswer;
@@ -39,76 +40,101 @@ public class Start extends Activity {
 	
 	private TextView questionTextView;
 	private TableLayout buttonTableLayout;
-	private TextView answerTextView;
+	private TextView answerTextView;           */
+	
+	List<Question> quesList;
+	int score=0;
+	int qid=0;
+	Question currentQ;
+	TextView txtQuestion;
+	RadioButton rda, rdb, rdc;
+	Button butNext;
+	//
+	int availableQuestions; // Questions 0 till (askable - 1) can be asked
+	Random random = new Random();
+	//
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.start);
+		setContentView(R.layout.activity_quiz);
 		
+
 		journey = MediaPlayer.create(Start.this, R.raw.journey);
 		journey.start();
 		
-		questionsList= new ArrayList<String>();
-		answersList = new ArrayList<String>();
-		regionsMap = new HashMap<String,Boolean>();
-		quessRows = 1;
-		random = new Random();
-		handler = new Handler();
+		DbHelper db=new DbHelper(this);
+		quesList=db.getAllQuestions();
+	//	currentQ=quesList.get(qid);
+		currentQ = pickQuestion();
+		//
+		txtQuestion=(TextView)findViewById(R.id.textView1);
+		rda=(RadioButton)findViewById(R.id.radio0);
+		rdb=(RadioButton)findViewById(R.id.radio1);
+		rdc=(RadioButton)findViewById(R.id.radio2);
+		butNext=(Button)findViewById(R.id.button1);
 		
-		String[]  questionsList = getResources().getStringArray(R.array.questionsList);
+		setQuestionView();
+		butNext.setOnClickListener(new View.OnClickListener() {		
+			@Override
+			public void onClick(View v) {
+				RadioGroup grp=(RadioGroup)findViewById(R.id.radioGroup1);
+				RadioButton answer=(RadioButton)findViewById(grp.getCheckedRadioButtonId());
+				Log.d("yourans", currentQ.getANSWER()+" "+answer.getText());
+				if(currentQ.getANSWER().equals(answer.getText()))
+				{
+					score++;
+					Log.d("score", "Your score"+score);
+				}
+				if(qid<5 ){					
+					currentQ=quesList.get(qid);
+					setQuestionView();
+				}else{
+					Intent intent = new Intent(Start.this, ResultActivity.class);
+					Bundle b = new Bundle();
+					b.putInt("score", score); //Your score
+					intent.putExtras(b); //Put your score to your next Intent
+					startActivity(intent);
+					finish();
+				}
+			}
+		});
 		
 		
-		questionTextView = (TextView) findViewById(R.id.questionTextView);
-		buttonTableLayout = (TableLayout)findViewById(R.id.buttonTableLayout);
-		answerTextView = (TextView)findViewById(R.id.answer);
+	}  //τέλος onCreate
+//
+	private Question pickQuestion() {
+		Collections.shuffle(quesList);
+		int counter=1;
+		availableQuestions = quesList.size();
+	while(counter <= availableQuestions )
+		  {
+		   currentQ = quesList.get(qid);
+		   counter++;
+			}
 		
-		questionTextView.setText(questionsList[0]);
-		
+		return currentQ;
+	}
+//
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_quiz, menu);
+		return true;
+	}
 
+
+	private void setQuestionView() {
+		
+		txtQuestion.setText(currentQ.getQUESTION());
+		rda.setText(currentQ.getOPTA());
+		rdb.setText(currentQ.getOPTB());
+		rdc.setText(currentQ.getOPTC());
+		qid++;
 		
 	}
 
 	
 
 	
-
-	
-	
-	
-	
-	
-	
-	@Override
-	protected void onDestroy() {
-		// TODO Auto-generated method stub
-		super.onDestroy();
-		journey.release();
-	}
-
-	@Override
-	protected void onPause() {
-		// TODO Auto-generated method stub
-		super.onPause();
-		journey.release();
-	}
-
-	@Override
-	protected void onResume() {
-		// TODO Auto-generated method stub
-		super.onResume();
-		journey.start();
-	}
-
-	
-
-	
-	
-
-	
-	
-	
-	
-	
-}
+} //end of Start Activity
