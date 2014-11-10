3dc34e09fa9e1c87728d3657e0e9d132063766c3
Maria <soulelemaria@hotmail.com>
2014-11-10 15:54:21 +0200

    db test of 5 questions working randomly redone

--- marias/src/com/example/marias/Exit.java
+++ marias/src/com/example/marias/Exit.java
@@ -5,12 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 
 public class Exit extends Activity{
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		this.finish();
+		
 	    Intent intent = new Intent(Intent.ACTION_MAIN);
 	    intent.addCategory(Intent.CATEGORY_HOME);
 	    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
